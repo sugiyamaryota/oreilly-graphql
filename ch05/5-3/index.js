c55a75f3bc0275ed5ab78f3a88d70ef2bae6cd1c
sugiyama ryota <guqeeky6va8@gmail.com>
2021-11-28 23:46:20 +0900

    fix

---
+++ ch05/5-3/index.js
@@ -0,0 +1,34 @@
+const {ApolloServer} = require(`apollo-server-express`)
+const express = require(`express`)
+const expressPlayground = require(`graphql-playground-middleware-express`).default
+const {readFileSync} = require(`fs`)
+
+const {MongoClient} = require(`mongodb`)
+require(`dotenv`).config()
+
+const typeDefs = readFileSync(`./typeDefs.graphql `, `UTF-8`)
+const resolvers = require(`./resolvers`)
+
+async function start() {
+    const app = express()
+    const MONGO_DB = process.env.DB_HOST
+    const client = await MongoClient.connect(
+        MONGO_DB,
+        {useNewUrlParser: true}
+    )
+    const db = client.db()
+    const context = {db}
+    const server = new ApolloServer({typeDefs, resolvers, context})
+
+    server.applyMiddleware({app})
+
+    app.get(`/playground`, expressPlayground({endpoint: `/graphql`}))
+
+    app.get(`/`, (req, res) => res.end(`welcome to the phtoshare api`))
+
+    app.listen({port: 4000}, () =>
+        console.log(`Graphql server running @ http://localhost:4000${server.graphqlPath}`)
+    )
+}
+
+start()
